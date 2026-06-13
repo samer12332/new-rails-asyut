f1fc01742ed39e5e0f3bd8fd270440d7c5624898
samer <sameryousry99@gmail.com>
2026-06-13 10:21:52 +0000

    Add User model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, :dob, :email, :phone_number, presence: true
+
+  validates :email,
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_13_083257) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_13_101740) do
   create_table "books", force: :cascade do |t|
     t.string "author"
     t.datetime "created_at", null: false
@@ -23,5 +23,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_13_083257) do
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.date "dob", null: false
+    t.string "email", null: false
+    t.string "name", null: false
+    t.string "phone_number", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
